ffde343a25424e3de27eff5d7236b5499cbac021
Kira Marston <kiramarston@gmail.com>
2020-03-21 13:26:38 +0000

    Added test and class templates

---
+++ specs/room_spec.rb
@@ -0,0 +1,27 @@
+require('minitest/autorun')
+require('minitest/reporters')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative('../song')
+require_relative('../customer')
+require_relative('../room')
+
+class TestRoom < Minitest::Test
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
